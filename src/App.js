3bc9dfebb3187bbc66553e0c54a6057053ae74d5
unknown <nenegrakrina@gmail.com>
2021-10-10 15:00:53 +0300

    create izza component + async getting pizzas (yes, fake backend)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Route } from 'react-router-dom';
 
 import { Header } from './components'
 import { Home, Cart } from './pages'
 
 function App() {
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((resp) => resp.json())
+      .then((json) => {
+        setPizzas(json.pizzas)
+    })
+  }, [])
+
   return (
     <div className="App">
       <div className="wrapper">
         <Header />
         <div className="content">
-          <Route path="/" component={Home} exact/>
+          <Route path="/" render={() => <Home items={pizzas} />} exact/>
           <Route path="/cart" component={Cart} exact/>
         </div>
       </div>
