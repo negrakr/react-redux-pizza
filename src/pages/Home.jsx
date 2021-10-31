aa0bccaddc79191604e78126dac0ebf2c58ca209
unknown <nenegrakrina@gmail.com>
2021-10-31 12:18:09 +0300

    cart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -68,7 +68,7 @@ function Home() {
               <PizzaBlock
                 onAddToCart={handleAddToCart}
                 key={obj.id}
-                addedCount={cartItems[obj.id] && cartItems[obj.id].length}
+                addedCount={cartItems[obj.id] && cartItems[obj.id].items.length}
                 {...obj}
               />  
             )
--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -13,7 +13,7 @@ export const fetchPizzas = (sortBy, category) => (dispatch) => {
 
   axios
     .get(
-      `http://localhost:3001/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${sortBy.order}`,
+      `/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${sortBy.order}`,
     )
     .then(({ data }) => {
       dispatch(setPizzas(data));
